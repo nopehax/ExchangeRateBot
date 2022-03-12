df84b8a8a74f76105d940edcf72eb1d13475adfc
nopehax <57407346+nopehax@users.noreply.github.com>
2022-03-12 22:35:01 +0800

    Update xchange.py
    now can handle non-message objects

--- xchange.py
+++ xchange.py
@@ -10,13 +10,16 @@ class update:
         response = requests.get(URL + 'getUpdates')
         j = response.json()
         self.success = j['ok']
-        
         result = j['result'][-1]
-        self.update_id = result['update_id']
-        
-        message = result['message']
-        self.chat_id = message['chat']['id']
-        self.text = message['text']
+        self.update_id = int(result['update_id'])
+
+        if 'message' not in result.keys():
+            self.isMessage = False
+        else:
+            self.isMessage = True
+            messageInfo = result['message']
+            self.chat_id = messageInfo['chat']['id']
+            self.text = messageInfo['text']
 
 class message:
     def __init__(self, chat):
@@ -27,7 +30,7 @@ class message:
         requests.post(url=self.link, data={'chat_id': self.chat_id, 'text': text}).json()
 
 def reset(offset):
-    requests.post(url=URL + 'getUpdates' + '?offset=%s' % offset).json()
+    requests.post(url=URL + 'getUpdates' + '?offset=%s' % str(offset + 1)).json()
 
 def convert(give, get):
     url = 'https://openexchangerates.org/api/latest.json?app_id=%s&symbols=%s,%s' % (X_TOKEN, give, get)
@@ -42,18 +45,17 @@ def convert(give, get):
     return '%.5f\nUpdated at %s' % (xrate, now)
 
 def action():
-    try:
-        get = update()
-    except (KeyError, IndexError):
-        reset(get.update_id + 1)
+    get = update()
+    if not get.isMessage:
+        reset(get.update_id)
         return
 
     command = get.text.startswith('/x')
-    reply = message(get.chat_id)
     if command == False:
-        reset(get.update_id + 1)
+        reset(get.update_id)
         return
 
+    reply = message(get.chat_id)
     arr = get.text.split()
     if len(arr) != 3:
         reply.this('the correct command is:\n/x FROM TO\ne.g. /x SGD AUD ')
@@ -62,7 +64,7 @@ def action():
             reply.this(convert(arr[1].upper(), arr[2].upper()))
         except KeyError:
             reply.this('pls input a valid currency')
-    reset(get.update_id + 1)
+    reset(get.update_id)
 
 while (True):
     print('\nbot is running...')
